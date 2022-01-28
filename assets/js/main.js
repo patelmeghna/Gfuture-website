6add4a3c63288e922a352e4246c08fc17201e5f1
meghnakidiya <meghnakidiya@gmail.com>
2022-01-28 19:09:27 +0530

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,68 +4,7 @@ function mobileMenu() {
   menu.classList.toggle("open");
 }
 
-// ================================= begin::typing text =============================
 
-const text = document.querySelector(".writting-text");
-const words = ["enerating"];
-
-// start typing effect
-setTyper(text, words);
-
-function setTyper(element, words) {
-  const LETTER_TYPE_DELAY = 120;
-  const WORD_STAY_DELAY = 1000;
-
-  const DIRECTION_FORWARDS = 0;
-  const DIRECTION_BACKWARDS = 1;
-
-  var direction = DIRECTION_FORWARDS;
-  var wordIndex = 0;
-  var letterIndex = 0;
-
-  var wordTypeInterval;
-
-  startTyping();
-
-  function startTyping() {
-    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
-  }
-
-  function typeLetter() {
-    const word = words[wordIndex];
-
-    if (direction == DIRECTION_FORWARDS) {
-      letterIndex++;
-
-      if (letterIndex == word.length) {
-        direction = DIRECTION_BACKWARDS;
-        clearInterval(wordTypeInterval);
-        setTimeout(startTyping, WORD_STAY_DELAY);
-      }
-    } else if (direction == DIRECTION_BACKWARDS) {
-      letterIndex--;
-
-      if (letterIndex == 0) {
-        nextWord();
-      }
-    }
-
-    const textToType = word.substring(0, letterIndex);
-
-    element.textContent = textToType;
-  }
-
-  function nextWord() {
-    letterIndex = 0;
-    direction = DIRECTION_FORWARDS;
-    wordIndex++;
-
-    if (wordIndex == words.length) {
-      wordIndex = 0;
-    }
-  }
-}
-// ================================= end::typing text =============================
 
 // ================================= begin::loader =============================
 $(window).on("load", function () {
@@ -74,7 +13,7 @@ $(window).on("load", function () {
 // ================================= end::loader =============================
 
 // ================================= begin::scroll reveal =============================
-window.addEventListener("scroll", reveal);
+// window.addEventListener("scroll", reveal);
 
 // function reveal() {
 //   var reveal = document.querySelectorAll('.reveal');
